5959847862c530bbe8f6702154f5df438dfcf085
masahiro331 <lomycisw@gmail.com>
2019-12-06 10:22:31 +0900

    wip

--- ch12/ex01/main.go
+++ ch12/ex01/main.go
@@ -4,12 +4,27 @@ import (
 	"github.com/masahiro331/colang/ch12/ex01/display"
 )
 
+type Hoge struct {
+	Name string
+}
+
 func main() {
 	display.Display("struct", struct {
-		Name string
-		Sex  string
+		Name  string
+		Sex   string
+		Array []string
+		Hoges []Hoge
 	}{
 		Name: "hoge",
 		Sex:  "man",
+		Array: []string{
+			"hoge",
+			"hoge",
+		},
+		Hoges: []Hoge{
+			{
+				Name: "hoge",
+			},
+		},
 	})
 }
