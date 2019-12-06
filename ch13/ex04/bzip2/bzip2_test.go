2f19fba28e0454fed78461f9e604bf05c9d4bcee
masahiro331 <lomycisw@gmail.com>
2019-12-06 14:44:41 +0900

    wip

--- ch13/ex04/bzip2/bzip2_test.go
+++ ch13/ex04/bzip2/bzip2_test.go
@@ -16,19 +16,19 @@ func TestBzip2(t *testing.T) {
 
 	tee := io.MultiWriter(w, &uncompressed)
 	for i := 0; i < 1000000; i++ {
-		io.WriteString(tee, "hello")
+		io.WriteString(tee, "hogehogeho")
 	}
 	if err := w.Close(); err != nil {
 		t.Fatal(err)
 	}
 
 	if got, want := compressed.Len(), 255; got != want {
-		t.Errorf("1 million hellos compressed to %d bytes, want %d", got, want)
+		t.Errorf("hoge compressed to %d bytes, want %d", got, want)
 	}
 
 	var decompressed bytes.Buffer
 	io.Copy(&decompressed, bzip2.NewReader(&compressed))
 	if !bytes.Equal(uncompressed.Bytes(), decompressed.Bytes()) {
-		t.Error("decompression yielded a different message")
+		t.Error("decompression different message")
 	}
 }
